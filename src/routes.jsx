298ae3db52a5ab591817687f98b95b7625f030e4
vinothsm12 <vinoth.sm12@gmail.com>
2025-04-17 12:10:25 +0530

    gh deploy fix

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom"; // Note: should be react-router-dom, not react-router
 import CoursePage from "./screens/course";
 import CourseDetails from "./screens/courseDetails";
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/cartedo">
       <Routes>
         <Route path="/" element={<CoursePage />} />
         <Route path="/course/:id" element={<CourseDetails />} />
